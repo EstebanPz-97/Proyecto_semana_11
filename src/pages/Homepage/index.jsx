7465c939dc2aff11ad6649bd412a1fb2be17ad33
esteban perez <esteban.perez@alteru.co>
2021-08-04 12:57:51 -0500

    Cree el formulario del login

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,9 +1,11 @@
-import "./homepage.scss"
+import "./homepage.scss";
+
 
 export const Homepage = () => {
  return (
     <div className="homepage">
         <h1>homepage</h1>
+
     </div>
  );
 };
